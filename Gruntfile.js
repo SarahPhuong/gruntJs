814d5da06d6ae7b0da1f21fd7065b1135bcccd73
DESKTOP-5DTE3VI\nguye <nguyenthanhphuong272@gmail.com>
2017-08-21 12:32:51 +0700

    create a Gruntfile.js and add a plugin

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports= (grunt)=>{
+    //Configuration
+    grunt.initConfig({
+        //pass in options to plugins, references to file  etc
+    concat:{
+        js:{
+            src:['js/*.js'],
+            dest:'build/scripts.js'
+        },
+        css:{
+            src:['css/*.css'],
+            dest:'build/style.css'
+        }
+    }
+    });
+
+    //Load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    //Register tasks
+    grunt.registerTask('concat-js',['concat:js']);
+    grunt.registerTask('concat-css',['concat:css'])
+    
+}
